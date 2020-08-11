bd52b81964d1db45d89778dfb4aaf4ce0b22e876
Kacper Ruściński <ruscin13@gmail.com>
2020-08-11 17:50:27 +0200

    redux sample code

--- src/index.js
+++ src/index.js
@@ -7,13 +7,31 @@ import allReducers from "./js/reducers/index"
 import { Provider } from "react-redux"
 import { useSelector } from "react-redux"
 
+const mongoose = require("mongoose");
+const uri = process.env.MONGO_CONNECTION_URL;
+const mongoConfig = {
+    useNewUrlParser: true,
+    userCreateIndex: true,
+
+}
+mongoose.connect(uri, mongoConfig);
+mongoose.connection.on("error", (err => {
+    console.log(err);
+    process.exit(1);
+}))
+
+mongoose.connection.on("connected", () => {
+    console.log("polaczona");
+
+})
+
 const store = createStore(allReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 ReactDOM.render(
     <Provider store={store}>
-       <Counter></Counter>
+        <Counter></Counter>
         <Form></Form>
-       
+
     </Provider>, document.getElementById("container"))
 
 
